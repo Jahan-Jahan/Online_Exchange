e78e1e6cbd03bc81d57fa980e1197371bcd20d60
AbolfazlJahangir <jahangirabolfazl@gmail.com>
2024-06-16 17:14:34 +0330

    Add title and icons

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module org.example.onlineexchange {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens org.example.onlineexchange to javafx.fxml;
--- src/main/java/org/example/onlineexchange/ExchangeApplication.java
+++ src/main/java/org/example/onlineexchange/ExchangeApplication.java
@@ -3,6 +3,7 @@ package org.example.onlineexchange;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,6 +13,7 @@ public class ExchangeApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(ExchangeApplication.class.getResource("signUp.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
+//        stage.getIcons().add(new Image("cryptoIcon.png"));
         stage.setTitle("Sign up");
         stage.setScene(scene);
         stage.show();
