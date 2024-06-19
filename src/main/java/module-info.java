f0676ee2dc3a752e2b29848b8fdc60cc2e61f8f3
AbolfazlJahangir <jahangirabolfazl@gmail.com>
2024-06-19 15:56:28 +0330

    Requirements and dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module org.example.onlineexchange {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires javax.mail.api;
 
 
     opens org.example.onlineexchange to javafx.fxml;
