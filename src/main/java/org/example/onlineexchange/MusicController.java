6b1ab83cbb1258d6366482664b6cc578dee5e98f
AbolfazlJahangir <jahangirabolfazl@gmail.com>
2024-06-23 04:15:39 +0330

    The music section added

---
+++ src/main/java/org/example/onlineexchange/MusicController.java
@@ -0,0 +1,30 @@
+package org.example.onlineexchange;
+
+import javafx.fxml.FXML;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+
+import java.util.Objects;
+
+public class MusicController {
+
+    @FXML
+    private MediaPlayer mediaPlayer;
+
+    public MusicController() {
+        String musicFile = "music-to-play/test.mp3";
+        Media sound = new Media(Objects.requireNonNull(getClass().getResource(musicFile)).toExternalForm());
+        mediaPlayer = new MediaPlayer(sound);
+    }
+
+    public void playMusic() {
+        if (mediaPlayer != null) {
+            mediaPlayer.play();
+        }
+    }
+    public void stopMusic() {
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+        }
+    }
+}
