da2b184ac01ef292ef996a65e2e0dc192b268a68
AbolfazlJahangir <jahangirabolfazl@gmail.com>
2024-06-23 04:04:06 +0330

    Add necessary requirements

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,11 @@
 module org.example.onlineexchange {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-    requires javax.mail.api;
-
+        requires javafx.controls;
+        requires javafx.fxml;
+        requires java.sql;
+        requires java.mail;
+        requires activation;
+        requires javafx.media;
 
     opens org.example.onlineexchange to javafx.fxml;
-    exports org.example.onlineexchange;
-}
+        exports org.example.onlineexchange;
+        }
