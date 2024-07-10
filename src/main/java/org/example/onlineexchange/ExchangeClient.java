a91dff9fd38cacc183015639ef23bcc1ddf923a5
AbolfazlJahangir <jahangirabolfazl@gmail.com>
2024-07-10 07:33:24 +0330

    Client

---
+++ src/main/java/org/example/onlineexchange/ExchangeClient.java
@@ -0,0 +1,33 @@
+package org.example.onlineexchange;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class ExchangeClient {
+    private static final String SERVER_ADDRESS = "localhost";
+    private static final int SERVER_PORT = 12345;
+
+    private Socket socket;
+    private PrintWriter out;
+    private BufferedReader in;
+
+    public ExchangeClient() throws IOException {
+        this.socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
+        this.out = new PrintWriter(socket.getOutputStream(), true);
+        this.in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    }
+
+    public String sendRequest(String request) throws IOException {
+        out.println(request);
+        return in.readLine();
+    }
+
+    public void close() throws IOException {
+        in.close();
+        out.close();
+        socket.close();
+    }
+}
